15734a0bc40befa1a8fa15b4b3f36fd2fb3e5dd5
sophiemiddleton <sophie@fnal.gov>
2019-12-14 10:26:19 -0600

    committing some changes

--- IPACaloCalib/src/IPACaloCalibAna_module.cc
+++ IPACaloCalib/src/IPACaloCalibAna_module.cc
@@ -76,24 +76,22 @@ namespace mu2e {
      public:
 
      struct Config {
-      using Name=fhicl::Name;
-      using Comment=fhicl::Comment;
-
-     fhicl::Atom<int> diagLevel{Name("diagLevel"),Comment("diag level"),0};
-     fhicl::Atom<std::string> g4ModuleLabel{Name("g4ModuleLabel"), Comment("g4 label"),"g4run"};
-     fhicl::Atom<std::string> generatorModuleLabel{Name("generatorModuleLabel"),Comment("gen label"),"generate"};
-     fhicl::Atom<std::string> caloReadoutModuleLabel{Name("caloReadoutModuleLabel"), Comment("readout"),"CaloReadoutHitsMaker"};
-     fhicl::Atom<std::string> caloCrystalModuleLabel{Name("caloCrystalModuleLabel"),Comment("cal label"),"CaloCrystalHitsMaker"};
-	fhicl::Atom<std::string> caloHitMCCrystalPtrLabel{Name("caloHitMCCrystalPtrLabel"),Comment("cal label"),"CaloHitMCCrystalPtr"};
-    fhicl::Atom<std::string> caloClusterPtrLabel{Name("caloClusterModuleLabel"),Comment("caloClusterModuleLabel"),"CaloClusterMakerNew"};
-    fhicl::Atom<std::string> virtualDetectorLabel{Name("virtualDetectorLabel"),Comment("virtualDetectorLabel"),"virtualdetector"};
-    fhicl::Atom<std::string> trkPatRecModuleLabel{Name("trkPatRecModuleLabel"),Comment("trkPatRecModuleLabel"),"TPRDownstreameMinus"};
+	      using Name=fhicl::Name;
+	      using Comment=fhicl::Comment;
+ 	      fhicl::Atom<int> diagLevel{Name("diagLevel"),Comment("diag level"),0};
+	     fhicl::Atom<int> mcdiag{Name("mcdiag"),Comment("diag level"),0};
+	     //fhicl::Atom<art::InputTag> calohitsTag{Name("CaloHitCollection"), Comment("readout")};
+	     fhicl::Atom<art::InputTag> calocrysTag{Name("CaloCrystalHitCollection"),Comment("cal reco crystal hit info")};
+	     //fhicl::Atom<art::InputTag> calohitMCcrysTag{Name("CaloHitMCTruthCollection"),Comment("cal hit mc truth info")};
+	     fhicl::Atom<art::InputTag> caloclusterTag{Name("CaloClusterCollection"),Comment("cal reco cluster info")};
+	     //fhicl::Atom<art::InputTag> caloSimPartMCTag{Name("CaloHitSimPartMCCollection"), Comment("cal hit sim particle mc info")};
     };
+       typedef art::EDAnalyzer::Table<Config> Parameters;
+
        typedef art::Ptr<StepPointMC> StepPtr;
        typedef std::vector<StepPtr>  StepPtrs;
        typedef std::map<int,StepPtrs > HitMap;
-       typedef art::EDAnalyzer::Table<Config> Parameters;
-
+       
        explicit IPACaloCalibAna(const Parameters& conf);
        virtual ~IPACaloCalibAna() {}
 
@@ -111,20 +109,19 @@ namespace mu2e {
 
        int _diagLevel;
        int _nProcess;
-
-       std::string _g4ModuleLabel;
-       std::string _generatorModuleLabel;
-       std::string _caloReadoutModuleLabel;
-       std::string _caloCrystalModuleLabel;
-       std::string _caloHitMCCrystalPtrLabel;
-       std::string _caloClusterModuleLabel;
-       std::string _caloClusterAlgorithm;
-       std::string _caloClusterSeeding;
-       const std::string _producerlabel;
-       std::string _virtualDetectorLabel;
-       std::string _trkPatRecModuleLabel;
-       std::string _instancelabel;
-
+	int _mcdiag;
+       //std::string _g4ModuleLabel;
+       //std::string _generatorModuleLabel;
+       art::InputTag _calohitsTag;
+       art::InputTag _calocrysTag;
+       art::InputTag _calohitMCcrysTag;
+       art::InputTag _caloclusterTag;
+       art::InputTag _caloSimPartMCTag;
+       const CaloHitCollection* _calhitcol;
+       const CaloCrystalHitCollection*  _calcryhitcol;
+       const CaloHitMCTruthCollection* _calhitMCtruecol;
+       const CaloClusterCollection* _calclustercol;
+       const CaloHitSimPartMCCollection* _simpartcol;
        TrkParticle _tpart;
        TrkFitDirection _fdir;
        
@@ -135,52 +132,48 @@ namespace mu2e {
 
        int   _evt,_run;
        int   _nHits;
-	int   _nSim;
-       int   _nGen,_genPdgId[16384],_genCrCode[16384];
-	float _genmomX[16384],_genmomY[16384],_genmomZ[16384],_genStartX[16384],_genStartY[16384],_genStartZ[16384],_genStartT[16384];
+       int   _nSim;
+       int   _nGen;
+       int   _cryId[16384],_crySectionId[16384],_crySimIdx[16384],_crySimLen[16384];
        float _cryTime[16384],_cryEdep[16384],_cryDose[16384],_cryPosX[16384],_cryPosY[16384],_cryPosZ[16384],_cryLeak[16384];
-       float _trueEdep[16384], _truetime[16384], trueCalX[16384] , trueCalY[16384] , trueCalZ[16384];
-  };
+       bool findData(const art::Event& evt);
+ };
 
 
   IPACaloCalibAna::IPACaloCalibAna(const Parameters& conf):
     art::EDAnalyzer(conf),
     _diagLevel(conf().diagLevel()),
-    _g4ModuleLabel(conf().g4ModuleLabel()),
-    _generatorModuleLabel(conf().generatorModuleLabel()),
-    _caloReadoutModuleLabel(conf().caloReadoutModuleLabel()),
-    _caloCrystalModuleLabel(conf().caloCrystalModuleLabel()),
-    _caloHitMCCrystalPtrLabel(conf().caloHitMCCrystalPtrLabel()),
-    _caloClusterModuleLabel(conf().caloClusterPtrLabel()),
-    _virtualDetectorLabel(conf().virtualDetectorLabel()),
-    _trkPatRecModuleLabel(conf().trkPatRecModuleLabel())
+    _mcdiag(conf().mcdiag()),
+    //_calohitsTag(conf().calohitsTag()),
+    _calocrysTag(conf().calocrysTag()),
+    //_calohitMCcrysTag(conf().calohitMCcrysTag()),
+    _caloclusterTag(conf().caloclusterTag())//,
+    //_caloSimPartMCTag(conf().caloSimPartMCTag())
 {}
 
   void IPACaloCalibAna::beginJob(){
 
     art::ServiceHandle<art::TFileService> tfs;
-
     _Ntup  = tfs->make<TTree>("CaloCalibAna", "CaloCalibAna");
-
-
-
     _Ntup->Branch("evt",          &_evt ,        "evt/I");
     _Ntup->Branch("run",          &_run ,        "run/I");
+    _Ntup->Branch("nCry",         &_nHits ,       "nCry/I");
+    _Ntup->Branch("cryId",        &_cryId ,       "cryId[nCry]/I");
+    _Ntup->Branch("crySectionId", &_crySectionId, "crySectionId[nCry]/I");
+    _Ntup->Branch("cryPosX",      &_cryPosX ,     "cryPosX[nCry]/F");
+    _Ntup->Branch("cryPosY",      &_cryPosY ,     "cryPosY[nCry]/F");
+    _Ntup->Branch("cryPosZ",      &_cryPosZ ,     "cryPosZ[nCry]/F");
+    _Ntup->Branch("cryEdep",      &_cryEdep ,     "cryEdep[nCry]/F");
+    _Ntup->Branch("cryTime",      &_cryTime ,     "cryTime[nCry]/F");
+    _Ntup->Branch("cryDose",      &_cryDose ,     "cryDose[nCry]/F");
+    _Ntup->Branch("crySimIdx",    &_crySimIdx ,   "crySimIdx[nCry]/I");
+    _Ntup->Branch("crySimLen",    &_crySimLen ,   "crySimLen[nCry]/I");
 
-    _Ntup->Branch("nGen",         &_nGen ,        "nGen/I");
-    _Ntup->Branch("genId",        &_genPdgId,     "genId[nGen]/I");
-    _Ntup->Branch("genCrCode",    &_genCrCode,    "genCrCode[nGen]/I");
-    _Ntup->Branch("genMomX",      &_genmomX,      "genMomX[nGen]/F");
-    _Ntup->Branch("genMomY",      &_genmomY,      "genMomX[nGen]/F");
-    _Ntup->Branch("genMomZ",      &_genmomZ,      "genMomX[nGen]/F");
-    _Ntup->Branch("genStartX",    &_genStartX,    "genStartX[nGen]/F");
-    _Ntup->Branch("genStartY",    &_genStartY,    "genStartY[nGen]/F");
-    _Ntup->Branch("genStartZ",    &_genStartZ,    "genStartZ[nGen]/F");
-    _Ntup->Branch("genStartT",    &_genStartT,    "genStartT[nGen]/F");
+    _hviewxy = tfs->make<TH2F>("viewxy","StepPoint MC hits dist xy",100,-20.,20.,100,-20.,20.);
+    _hviewxz = tfs->make<TH2F>("viewxz","StepPoint MC hits dist xz",230,-10.,220.,100,-20.,20.);
+   
 
   }
-
-	
 
   void IPACaloCalibAna::endJob(){
   }
@@ -189,7 +182,12 @@ namespace mu2e {
 
 
   void IPACaloCalibAna::analyze(const art::Event& event) {
-
+	
+       _evt = event.id().event();
+       _run = event.run();
+       if(!findData(event)) // find data
+      		throw cet::exception("RECO")<<"No data in  event"<< endl; 
+           
       ++_nProcess;
       if (_nProcess%1000==0) std::cout<<"Processing event "<<_nProcess<<std::endl;
       
@@ -198,67 +196,17 @@ namespace mu2e {
       art::ServiceHandle<GeometryService> geom;
       if( ! geom->hasElement<Calorimeter>() ) return;
       Calorimeter const & cal = *(GeomHandle<Calorimeter>());
-  
-	/*
-      //Get generated particles
-      art::Handle<GenParticleCollection> gensHandle;
-      event.getByLabel(_generatorModuleLabel, gensHandle);
-      GenParticleCollection const& genParticles(*gensHandle);
-*/
-      //Get calorimeter readout hits (2 readout / crystal as of today)
-      art::Handle<CaloHitCollection> caloHitsHandle;
-      event.getByLabel(_caloReadoutModuleLabel, caloHitsHandle);
-      CaloHitCollection const& caloHits(*caloHitsHandle);
-      
-      //Get calorimeter readout hits MC level - energy/time/type
-      art::Handle<CaloHitMCTruthCollection> caloHitMCTruthHandle;
-      event.getByLabel(_caloReadoutModuleLabel, caloHitMCTruthHandle);
-      CaloHitMCTruthCollection const& caloHitsMCTruth(*caloHitMCTruthHandle);
-      
-      
-      //Get simParticles and stepPointMC summary for crystal readout hits
-      art::Handle<CaloHitSimPartMCCollection> caloHitSimMCHandle;
-      event.getByLabel(_caloReadoutModuleLabel, caloHitSimMCHandle);
-      CaloHitSimPartMCCollection const& caloHitSimPartMC(*caloHitSimMCHandle);
-            
-      //Get calo crystal hits (average from readouts)
-      art::Handle<CaloCrystalHitCollection> caloCrystalHitsHandle;
-      event.getByLabel(_caloCrystalModuleLabel, caloCrystalHitsHandle);
-      CaloCrystalHitCollection const& caloCrystalHits(*caloCrystalHitsHandle);
-
-   
-      //Utility to match  cloHits with MCtruth, simParticles and StepPoints
-      CaloHitMCNavigator caloHitNavigator(caloHits, caloHitsMCTruth, caloHitSimPartMC);
+  //Utility to match  cloHits with MCtruth, simParticles and StepPoints
+      //CaloHitMCNavigator caloHitNavigator(caloHits, caloHitsMCTruth, caloHitSimPartMC);
    //--------------------------  Do generated particles --------------------------------
-
-       _evt = event.id().event();
-       _run = event.run();
-              /*
-       _nGen = genParticles.size();
-	if(_nGen !=0){
-	       for (unsigned int i=0; i < genParticles.size(); ++i)
-	       {
-		   GenParticle const* gen = &genParticles[i];
-		   _genPdgId[i]   = gen->pdgId();
-		   _genCrCode[i]  = gen->generatorId().id();
-		   _genmomX[i]    = gen->momentum().vect().x();
-		   _genmomY[i]    = gen->momentum().vect().y();
-		   _genmomZ[i]    = gen->momentum().vect().z();
-		   _genStartX[i]  = gen->position().x()+ 3904;
-		   _genStartY[i]  = gen->position().y();
-		   _genStartZ[i]  = gen->position().z();
-		   _genStartT[i]  = gen->time();
-	       } 
-      }
-      
-      */
+ 
        //--------------------------  Do calorimeter hits --------------------------------
       
        _nHits = _nSim = 0;
     
-	for (unsigned int ic=0; ic<caloCrystalHits.size();++ic) 
+	for (unsigned int ic=0; ic<_calcryhitcol->size();++ic) 
        {	   
-	   CaloCrystalHit const& hit      = caloCrystalHits.at(ic);
+	   CaloCrystalHit const& hit      = (*_calcryhitcol)[ic];
 	   int diskId                     = cal.crystal(hit.id()).diskId();
            CLHEP::Hep3Vector crystalPos   = cal.geomUtil().mu2eToDiskFF(diskId,cal.crystal(hit.id()).position());  //in disk FF frame
            //CaloHit const& caloHit         = *(hit.readouts().at(0));
@@ -277,6 +225,33 @@ namespace mu2e {
   
 
   }
+
+bool IPACaloCalibAna::findData(const art::Event& evt){
+
+	_calhitcol =0;
+	_calcryhitcol =0;
+	_calclustercol=0;
+	//auto calhit = evt.getValidHandle<CaloHitCollection>(_calohitsTag);
+	//_calhitcol = calhit.product();
+	auto cryhit = evt.getValidHandle<CaloCrystalHitCollection>(_calocrysTag);
+	_calcryhitcol =cryhit.product();
+	auto cluster= evt.getValidHandle<CaloClusterCollection>(_caloclusterTag);
+	_calclustercol =cluster.product();
+        if(_mcdiag){
+	    
+	   _calhitMCtruecol=0;
+	   _simpartcol = 0;
+           auto simpar= evt.getValidHandle<CaloHitSimPartMCCollection>(_caloSimPartMCTag);
+	   _simpartcol =simpar.product();
+	   auto calhitMC = evt.getValidHandle<CaloHitMCTruthCollection>(_calohitMCcrysTag);
+	   _calhitMCtruecol =calhitMC.product();
+           //_toff.updateMap(evt);
+        }
+	return _calcryhitcol!=0 && _calclustercol !=0 && (_simpartcol != 0 || !_mcdiag);//_calhitcol != 0 && 
+       }
+
+//}
+
 }  // end namespace mu2e
 
 DEFINE_ART_MODULE(mu2e::IPACaloCalibAna);
