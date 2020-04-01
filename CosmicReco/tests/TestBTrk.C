3913cbef17e29f8411419aaa03f1701b2b261813
sophiemiddleton <sophie@fnal.gov>
2020-04-01 08:50:49 -0500

    updated test

--- CosmicReco/tests/TestBTrk.C
+++ CosmicReco/tests/TestBTrk.C
@@ -1,5 +1,5 @@
 // Tests to validate the CosmicBTrk Algorithms
-//
+//Usage: ROOT - .L Test.C , TVector3 start(0,0,0), TVector3(1,2,3),
 #include "TH1F.h"
 #include "TSystem.h"
 #include "THelix.h"
@@ -33,16 +33,16 @@ using namespace ROOT::Math;
 
 
 TVector3 GetPOCA( TVector3 point,TVector3 start,TVector3 end){
-	  
+
     double tMin = -(start-point).Dot(end-start) /((end-start).Mag2());
 	  double POCA_x = start.x() + (end.x()-start.x())*tMin;
 	  double POCA_y = start.y() + (end.x()-start.y())*tMin;
 	  double POCA_z = start.z() + (end.z()-start.z())*tMin;
 	  TVector3 closestPointOnLine;
 	  closestPointOnLine.SetXYZ(POCA_x ,POCA_y ,POCA_z);
-	 
+
 	 return closestPointOnLine;
-	 
+
 }
 
 double GetDOCA(TVector3 closestPointOnLine, TVector3 point){
@@ -50,14 +50,14 @@ double GetDOCA(TVector3 closestPointOnLine, TVector3 point){
 
 }
 
-TVector3 direction(double phi0, double theta) 
+TVector3 direction(double phi0, double theta)
 {
 	TVector3 Dir;
 	Dir.SetXYZ(cos(phi0)*sin(theta),sin(theta)*sin(phi0), cos(theta));
-        return Dir;
+  return Dir;
 }
 
-TVector3 position(double f, double phi0, double theta, double z0, double d0, TVector3 Pos0)  
+TVector3 position(double f, double phi0, double theta, double z0, double d0, TVector3 Pos0)
 {
 	double sphi0 = sin(phi0);
 	double cphi0 = cos(phi0);
@@ -69,8 +69,6 @@ TVector3 position(double f, double phi0, double theta, double z0, double d0, TVe
 	return Pos;
 }
 
-
-
 void TestBTrk( TVector3 Start, TVector3 End) {
 	TVector3 Pos0;
 	Pos0.SetXYZ(0,0,0); //Reference point is set at 0.
@@ -78,13 +76,13 @@ void TestBTrk( TVector3 Start, TVector3 End) {
 	unsigned AMSIGN = copysign(1,POCA.X());
 	double DOCA = GetDOCA(POCA, Pos0);
 	TVector3 TrackDirection;
-	TrackDirection = End-Start;
+	TrackDirection = End - Start;
 	//TrackDirection.SetXYZ(TrackDirection.X()/TrackDirection.Z(),TrackDirection.Y()/TrackDirection.Z(),1);
-	double theta =asin(TrackDirection.Y()/(TrackDirection.Mag())); 
+	double theta =asin(TrackDirection.Y()/(TrackDirection.Mag()));
 	double phi0 = AMSIGN*atan2(POCA.X(),POCA.Y());
 	double z0  = POCA.Z();
 	double d0 = DOCA;
-  
+
 	cout << "Cosmic Test Parameter:" << endl
 	<< "Theta = " <<theta << endl
 	<< "phi-0 = " << phi0 << endl
@@ -103,96 +101,152 @@ void TestBTrk( TVector3 Start, TVector3 End) {
 	TVector3 PosEndTest = TestPosition + f*TestDirection;
 	cout<<"start position is: "<<Start.X()<<" "<<Start.Y()<<" "<<Start.Z()<<endl;
 	cout<<"test position is:"<<PosEndTest.X()<<" "<<PosEndTest.Y()<<" "<<PosEndTest.Z()<<endl;
-/*
-  TCanvas* hcan = new TCanvas("hcan","Cosmics",1000,1000);
-//TPolyLine to graph the result
-  TPolyLine3D* hel = new TPolyLine3D(nsteps+1);
-  TPolyLine3D* invhel = new TPolyLine3D(nsteps+1);
 
-  for(int istep=0;istep<nsteps+1;++istep){
-  
-    hel->SetPoint(istep, hpos._x, hpos._y, hpos._z);
-    HelixPos(invpars,hpos);
-    invhel->SetPoint(istep, hpos._x, hpos._y, hpos._z);
-  }
-  // draw the helix
-  if(charge > 0)
-    hel->SetLineColor(kBlue);
-  else
-    hel->SetLineColor(kRed);
-  hel->Draw();
-
-  // draw the origin and axes
-  TAxis3D* rulers = new TAxis3D();
-  rulers->GetXaxis()->SetAxisColor(kBlue);
-  rulers->GetXaxis()->SetLabelColor(kBlue);
-  rulers->GetYaxis()->SetAxisColor(kCyan);
-  rulers->GetYaxis()->SetLabelColor(kCyan);
-  rulers->GetZaxis()->SetAxisColor(kOrange);
-  rulers->GetZaxis()->SetLabelColor(kOrange);
-  rulers->Draw();
-
-  TPolyLine3D* refmom = new TPolyLine3D(2);
-  refmom->SetPoint(0,pos._x,pos._y,pos._z);
-  double mommag = sqrt(mom._x*mom._x + mom._y*mom._y + mom._z*mom._z);
-  double momscale = fabs(pars._pars[_rad])/mommag;
-  refmom->SetPoint(1,pos._x + mom._x*momscale,pos._y + mom._y*momscale ,pos._z + mom._z*momscale);
-  int momcolor;
-  if(charge>0.0)
-    momcolor = kRed;
-  else
-    momcolor = kBlack;
-  refmom->SetLineColor(momcolor);
-  refmom->Draw();
-
-  TPolyMarker3D* refp = new TPolyMarker3D(1,24);
-  refp->SetMarkerColor(momcolor);
-  refp->SetPoint(0,pos._x,pos._y,pos._z);
-  refp->Draw();
-  TPolyMarker3D* refmomp = new TPolyMarker3D(1,22);
-  refmomp->SetPoint(0,pos._x + mom._x*momscale,pos._y + mom._y*momscale ,pos._z + mom._z*momscale);
-  refmomp->SetMarkerColor(momcolor);
-  refmomp->Draw();
-
-  TPolyMarker3D* helixp = new TPolyMarker3D(1,3);
-  helixp->SetMarkerColor(kGreen);
-  hpos._t = pos._t;
-  HelixPos(pars,hpos);
-  helixp->SetPoint(0,hpos._x,hpos._y,hpos._z);
-  helixp->Draw();
-
-  TPolyMarker3D* testp = new TPolyMarker3D(1,25);
-  testp->SetMarkerColor(kOrange);
-  testp->SetPoint(0,hpos._x,hpos._y,hpos._z+2*M_PI*pars._pars[_lambda]);
-  testp->Draw();
-
-  TPolyMarker3D* startp = new TPolyMarker3D(1,21);
-  startp->SetMarkerColor(kBlue);
-  hpos._t = tmin;
-  HelixPos(pars,hpos);
-  startp->SetPoint(0,hpos._x,hpos._y,hpos._z);
-  startp->Draw();
-
-  TPolyMarker3D* endp = new TPolyMarker3D(1,22);
-  endp->SetMarkerColor(kBlue);
-  hpos._t = tmax;
-  HelixPos(pars,hpos);
-  endp->SetPoint(0,hpos._x,hpos._y,hpos._z);
-  endp->Draw();
-
-  TLegend* leg = new TLegend(0.8,0.8,1.0,1.0);
-  char title[80];
-  snprintf(title,80,"Helix, mass=%3.1g MeV/c^{2}, q=%1.1f",mass,charge);
-  leg->AddEntry(hel,title,"L");
-  snprintf(title,80,"Initial Momentum =%3.1g MeV/c",momval);
-  leg->AddEntry(refmom,title,"L");
-  leg->AddEntry(refp,"Initial Position","P");
-  snprintf(title,80,"Helix, t=%4.2g ns",pos._t);
-  leg->AddEntry(helixp,title,"P");
-  snprintf(title,80,"Helix, t=%4.2g ns",pos._t+tmin);
-  leg->AddEntry(startp,title,"P");
-  snprintf(title,80,"Helix, t=%4.2g ns",pos._t+tmax);
-  leg->AddEntry(endp,title,"P");
-  leg->Draw();
-*/
 }
+
+void derivDeflect(double fltlen, double theta, double d0, int opt)
+{
+
+  double thetaIndex;
+  double d0Index;
+  double phi0Index;
+  double z0Index;
+  if(opt == 1){
+
+    thetaIndex = 1;
+    d0Index = 0;
+    phi0Index = 0;
+    z0Index = -1*fltlen*(1/sin(theta));
+  }
+  if(opt == 2){
+
+    thetaIndex = 0;
+    d0Index = -1*fltlen;
+    phi0Index = 1/sin(theta);
+    z0Index = -d0*1/(sin(theta)*tan(theta));
+
+  }
+  std::cout<<"Theta "<<thetaIndex<<" d0 "<<d0Index<<endl
+  <<" z0 "<<z0Index<<" phi 0 "<<phi0Index<<std::endl;
+}
+
+
+void calcCosmicLineAllCovs(double fltlen, double theta, double phi0, double z0, double d0) {
+
+  double sinphi0 = sin(phi0);
+  double cosphi0 = cos(phi0);
+
+  double costheta = cos(theta);
+  double sintheta = sin(theta);
+
+  TrkMomVisitor theVisitor(theTraj);
+  double pt =0;//TODO
+
+  double d_x_d0 = -1*sinphi0;
+  double d_x_phi0 = -d0*cosphi0-fltlen*sintheta*sinphi0;
+  double d_x_theta = fltlen*cosphi0*costheta;
+  double d_x_z0 = 0;
+
+  double d_y_d0 = cosphi0;
+  double d_y_phi0 = -1*d0*sinphi0+fltlen*sintheta*cosphi0;
+  double d_y_theta = fltlen*sinphi0*costheta;
+  double d_y_z0 = 0;
+
+  double d_z_theta = -1*fltlen*sintheta;
+  double d_z_z0 = 1;
+  double d_z_d0 = 0;
+  double d_z_phi0 = 0;
+
+  double d_px_phi0 = -pt*sinphi0*sintheta;
+  double d_py_phi0 = pt*cosphi0*sintheta;
+  double d_px_theta = pt*cosphi0*costheta;
+  double d_py_theta = pt*sinphi0*costheta;
+  double d_pz_phi0 = 0;
+  double d_pz_theta = -1*pt*sintheta;
+  double d_pz_z0 = 1;
+
+  double m_d0_d0 =  1;
+  double m_d0_phi0 = 0;
+  double m_d0_theta =  0;
+  double m_d0_z0 = 0;
+
+  double m_phi0_phi0 =  1;
+  double m_phi0_z0 =  0;
+  double m_phi0_theta =  0;
+
+  double m_theta_theta =  1;
+  double m_z0_theta =  0;
+  double m_z0_z0 =  1;
+
+  double xxCov11 =
+    d_x_d0* (  d_x_d0*m_d0_d0 + d_x_phi0*m_d0_phi0 + d_x_theta*m_d0_theta + d_x_z0*m_d0_z0  )   +
+    d_x_phi0* (  d_x_d0*m_d0_phi0 + d_x_phi0*m_phi0_phi0 + d_x_theta*m_phi0_theta + d_x_z0*m_phi0_z0  )   +
+    d_x_theta* (  d_x_d0*m_d0_theta + d_x_phi0*m_phi0_theta + d_x_theta*m_theta_theta + d_x_z0*m_z0_theta  )   +
+    d_x_z0* (  d_x_d0*m_d0_z0 + d_x_phi0*m_phi0_z0 + d_x_theta*m_phi0_theta + d_x_z0*m_z0_z0  )  ;
+  double xxCov21 =
+    d_y_d0* (  d_x_d0*m_d0_d0 + d_x_phi0*m_d0_phi0 + d_x_theta*m_d0_theta + d_x_z0*m_d0_z0  )   +
+    d_y_phi0* (  d_x_d0*m_d0_phi0 + d_x_phi0*m_phi0_phi0 + d_x_theta*m_phi0_theta + d_x_z0*m_phi0_z0  )   +
+    d_y_theta* (  d_x_d0*m_d0_theta + d_x_phi0*m_phi0_theta + d_x_theta*m_theta_theta + d_x_z0*m_z0_theta  )   +
+    d_y_z0* (  d_x_d0*m_d0_z0 + d_x_phi0*m_phi0_z0 + d_x_theta*m_z0_theta + d_x_z0*m_z0_z0  )   ;
+  double xxCov22 =
+    d_y_d0* (  d_y_d0*m_d0_d0 + d_y_phi0*m_d0_phi0 + d_y_theta*m_d0_theta + d_y_z0*m_d0_z0  )   +
+    d_y_phi0* (  d_y_d0*m_d0_phi0 + d_y_phi0*m_phi0_phi0 + d_y_theta*m_phi0_theta + d_y_z0*m_phi0_z0  )   +
+    d_y_theta* (  d_y_d0*m_d0_theta + d_y_phi0*m_phi0_theta + d_y_theta*m_theta_theta + d_y_z0*m_z0_theta  )   +
+    d_y_z0* (  d_y_d0*m_d0_z0 + d_y_phi0*m_phi0_z0 + d_y_theta*m_z0_theta + d_y_z0*m_z0_z0  )   ;
+
+  double xxCov31 =
+    d_z_d0*(  d_x_d0*m_d0_d0 + d_x_phi0*m_d0_theta + d_x_theta*m_d0_theta + d_x_z0*m_d0_theta )+
+    d_z_phi0*(  d_x_d0*m_d0_phi0 + d_x_phi0*m_phi0_phi0 + d_x_theta*m_phi0_theta + d_x_z0*m_phi0_z0 )+d_z_theta*(  d_x_d0*m_d0_theta + d_x_phi0*m_phi0_theta + d_x_theta*m_theta_theta + d_x_z0*m_z0_theta )+
+   d_z_z0*(  d_x_d0*m_d0_phi0 + d_x_phi0*m_phi0_z0 + d_x_theta*m_z0_theta + d_x_z0*m_z0_z0 );
+
+  double xxCov32 = d_z_d0*(  d_y_d0*m_d0_d0 + d_y_phi0*m_d0_theta + d_y_theta*m_d0_theta + d_y_z0*m_d0_theta )+
+    d_z_phi0*(  d_y_d0*m_d0_phi0 + d_y_phi0*m_phi0_phi0 + d_y_theta*m_phi0_theta + d_y_z0*m_phi0_z0 )+d_z_theta*(  d_y_d0*m_d0_theta + d_y_phi0*m_phi0_theta + d_y_theta*m_theta_theta + d_y_z0*m_z0_theta )+
+   d_z_z0*(  d_y_d0*m_d0_phi0 + d_y_phi0*m_phi0_z0 + d_y_theta*m_z0_theta + d_y_z0*m_z0_z0 );
+
+  double xxCov33 = d_z_d0*(  d_z_d0*m_d0_d0 + d_z_phi0*m_d0_theta + d_z_theta*m_d0_theta + d_z_z0*m_d0_theta )+
+    d_z_phi0*(  d_z_d0*m_d0_phi0 + d_z_phi0*m_phi0_phi0 + d_z_theta*m_phi0_theta + d_z_z0*m_phi0_z0 )+d_z_theta*(  d_z_d0*m_d0_theta + d_z_phi0*m_phi0_theta + d_z_theta*m_theta_theta + d_z_z0*m_z0_theta )+
+   d_z_z0*(  d_z_d0*m_d0_phi0 + d_z_phi0*m_phi0_z0 + d_z_theta*m_z0_theta + d_z_z0*m_z0_z0 );
+
+
+  double xpCov11 = d_px_theta* (  d_x_d0*m_d0_theta + d_x_phi0*m_phi0_theta + d_x_theta*m_theta_theta + d_x_z0*m_z0_theta  )  +
+ d_px_phi0* (  d_x_d0*m_d0_phi0 + d_x_phi0*m_phi0_phi0 + d_x_theta*m_phi0_theta + d_x_z0*m_phi0_z0  ) ;
+
+  double xpCov21 = d_px_theta* (  d_y_d0*m_d0_theta + d_y_phi0*m_phi0_theta + d_y_theta*m_theta_theta + d_y_z0*m_z0_theta  )   +
+    d_px_phi0* (  d_y_d0*m_d0_phi0 + d_y_phi0*m_phi0_phi0 + d_y_theta*m_phi0_theta + d_y_z0*m_phi0_z0  )   ;
+
+  double xpCov31 = d_px_theta* (  d_z_d0*m_d0_theta + d_z_phi0*m_phi0_theta + d_z_theta*m_theta_theta + d_z_z0*m_z0_theta  )   +
+    d_px_phi0* (  d_z_d0*m_d0_phi0 + d_z_phi0*m_phi0_z0 + d_z_theta*m_phi0_theta + d_z_z0*m_phi0_z0  )   ;
+
+  double xpCov12 =  d_py_theta* (  d_x_d0*m_d0_theta + d_x_phi0*m_phi0_theta + d_x_theta*m_theta_theta + d_x_z0*m_z0_theta  )   +
+    d_py_phi0* (  d_x_d0*m_d0_phi0 + d_x_phi0*m_phi0_phi0 + d_x_theta*m_phi0_theta + d_x_z0*m_phi0_z0 )   ;
+
+  double xpCov22 =  d_py_theta* (  d_y_d0*m_d0_theta + d_y_phi0*m_phi0_theta + d_y_theta*m_theta_theta + d_y_z0*m_z0_theta  )   +
+    d_py_phi0* (  d_y_d0*m_d0_phi0 + d_y_phi0*m_phi0_phi0 + d_y_theta*m_phi0_theta + d_y_z0*m_phi0_z0 )   ;
+
+  double xpCov32 =  d_py_theta* (  d_z_d0*m_d0_theta + d_z_phi0*m_phi0_theta + d_z_theta*m_theta_theta + d_y_z0*m_z0_theta  )   +
+    d_py_phi0* (  d_z_d0*m_d0_phi0 + d_z_phi0*m_phi0_phi0 + d_z_theta*m_phi0_theta + d_z_z0*m_phi0_z0  )   ;
+
+  double xpCov13 =  d_pz_theta* (  d_x_d0*m_d0_theta + d_x_phi0*m_phi0_theta + d_x_theta*m_theta_theta + d_x_z0*m_z0_theta  )   +
+    d_pz_phi0* (  d_x_d0*m_d0_phi0 + d_x_phi0*m_phi0_phi0 + d_x_theta*m_phi0_theta + d_x_z0*m_phi0_z0  )   ;
+
+  double xpCov23 =  d_pz_theta* (  d_y_d0*m_d0_theta + d_y_phi0*m_phi0_theta + d_y_theta*m_theta_theta + d_y_z0*m_z0_theta  )   +
+    d_pz_phi0* (  d_y_d0*m_d0_phi0 + d_y_phi0*m_phi0_z0 + d_y_theta*m_phi0_theta + d_y_z0*m_phi0_z0 )   ;
+
+  double xpCov33 =  d_pz_theta* (  d_z_d0*m_d0_theta + d_z_phi0*m_phi0_theta + d_z_theta*m_theta_theta + d_z_z0*m_z0_theta  )   +
+    d_pz_phi0* (  d_z_d0*m_d0_phi0 + d_z_phi0*m_phi0_phi0 + d_z_theta*m_phi0_theta + d_z_z0*m_phi0_z0)   ;
+
+
+  double ppCov11 = d_px_theta* ( d_px_theta*m_theta_theta + d_px_phi0*m_phi0_theta  )   ;
+
+  double ppCov21 = d_py_theta* ( d_px_theta*m_theta_theta + d_px_phi0*m_phi0_theta  );
+
+  double ppCov22 = d_py_theta* ( d_py_theta*m_theta_theta + d_py_phi0*m_phi0_theta  );
+
+  double ppCov31 = d_pz_theta* ( d_px_theta*m_theta_theta + d_px_phi0*m_phi0_theta  ) ;
+
+  double ppCov32 = d_pz_theta* ( d_py_theta*m_theta_theta + d_py_phi0*m_phi0_theta  ) ;
+
+  double ppCov33 = d_pz_theta* ( d_pz_theta*m_theta_theta  ) + d_pz_z0*(d_pz_z0*m_z0_z0) ;
+
+}
+
