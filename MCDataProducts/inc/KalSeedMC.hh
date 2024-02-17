559665394664c83f3214dde4fba2f558598cfc66
sophieMu2e <sophie@fnal.gov>
2024-02-17 11:59:56 -0600

    stash

--- MCDataProducts/inc/KalSeedMC.hh
+++ MCDataProducts/inc/KalSeedMC.hh
@@ -30,12 +30,14 @@ namespace mu2e {
     typedef art::Handle<SimParticleCollection> SPCH;
     PDGCode::type _pdg; // code of this particle
     ProcessCode _proc; // particle creation process
+    ProcessCode _stop; // particle stopping process
     GenId _gid; // generator code
     MCRelationship _rel; // relationship of this particle to its primary
     uint16_t _nhits; // number of associated StrawHits
     uint16_t _nactive; // number of associated active hits
     XYZTVectorF   _mom; // initial momentum
     XYZVectorF _pos; // initial position
+    XYZVectorF _endpos; // end position
     double _time; //global start time
     cet::map_vector_key _spkey; // key to the SimParticle
     // construct a Ptr from Handle and key
@@ -44,7 +46,7 @@ namespace mu2e {
     // partial constructor from a SimParticle;
     SimPartStub(SPPtr const& spp)  : _pdg(spp->pdgId()),
     _proc(spp->creationCode()), _gid(GenId::unknown), _rel(MCRelationship::none),
-    _nhits(0), _nactive(0), _mom(CLHEP::HepLorentzVector(spp->startMomentum())),  _pos(XYZVectorF(spp->startPosition())), _time(spp->startGlobalTime()), _spkey(spp.key()){
+    _nhits(0), _nactive(0), _mom(CLHEP::HepLorentzVector(spp->startMomentum())),  _pos(XYZVectorF(spp->startPosition())),_endpos(XYZVectorF(spp->endPosition())), _time(spp->startGlobalTime()), _spkey(spp.key()){
     // dig down to the GenParticle
       auto simPtr = spp;
       while (simPtr->genParticle().isNull() && simPtr->parent().isNonnull()) {
